0b09d1ad17d3ca533f95fd32bc8f5355b1bdf76a
Ennilavan <ennilavan13@gmail.com>
2026-06-17 12:32:15 +0530

    corrected error

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
